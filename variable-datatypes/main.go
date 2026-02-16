2977b2ccdfdad11fb2e7398e551ffb467e6373c0
Sagor Majomder <sagormajomder@gmail.com>
2026-02-17 01:50:16 +0600

    Learn about varialbe and datatype

---
+++ variable-datatypes/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+// Variable
+	// var a int = 10
+	// var a = 10
+	// a := 40.5
+	// a := "Hello World"
+	a := true
+	a = false
+
+	const p = 100
+
+	fmt.Println(a)
+	fmt.Println(p)
+}
