a0672e1bb0a26d8112b2ecb3591ff65ca3f771ca
Sagor Majomder <sagormajomder@gmail.com>
2026-03-06 09:46:39 +0600

    Learn go array

---
+++ 15-arrays/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+var arr3 = [3]string{"I","Love","You"}
+
+
+func main() {
+	// var arr [2]int
+	// arr[0]=1
+	// fmt.Println(arr)
+
+	arr2:= [2]int{3,4}
+	fmt.Println(arr2)
+	fmt.Println(arr3)
+	fmt.Println(arr3[1])
+
+
+}
