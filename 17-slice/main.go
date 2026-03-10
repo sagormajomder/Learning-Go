0c61cabfbe8f920354e0d028207e16dc2163aefe
Sagor Majomder <sagormajomder@gmail.com>
2026-03-10 09:19:37 +0600

    Add README and main.go examples for understanding slices in Go

--- 17-slice/main.go
+++ 17-slice/main.go
@@ -56,6 +56,60 @@ func main() {
 	fmt.Println("Length: ",len(emptySlice))
 	fmt.Println("Capacity: ",cap(emptySlice))
 
+	fmt.Println("=========================")
+	var x []int  // [], len = 0, cap = 0
+	x = append(x, 1) // [1], len = 1, cap = 1 
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 2)
+	fmt.Println(x, len(x), cap(x))
+	x = append(x, 3)
+	fmt.Println(x, len(x), cap(x))
+	y:= x 
+	fmt.Println("Y: ",y, len(y), cap(y))
+
+	x = append(x, 4)
+	fmt.Println("X: ",x, len(x), cap(x))
+	fmt.Println("Y: ",y, len(y), cap(y))
+	// Slice underlying array rule => 1024 -> 100% increase, 1024 < 25% increase
+	y = append(y, 5)
+	fmt.Println("X: ",x, len(x), cap(x))
+	fmt.Println("Y: ",y, len(y), cap(y))
+
+	x[0] = 10
+	fmt.Println("X: ",x, len(x), cap(x))
+	fmt.Println("Y: ",y, len(y), cap(y))
+	fmt.Println("=========================")
+
+	newSlice := []int{1,2,3,4,5} // [1,2,3,4,5], len = 5, cap = 5
+ 	fmt.Println(newSlice, len(newSlice), cap(newSlice)) 
+	newSlice = append(newSlice, 6) // [1,2,3,4,5,6], len=6,cap=10
+	fmt.Println(newSlice, len(newSlice), cap(newSlice)) 
+	newSlice = append(newSlice, 7) // [1,2,3,4,5,6,7], len=7,cap=10
+	fmt.Println(newSlice, len(newSlice), cap(newSlice))
+
+	a:= newSlice[4:] // [5,6,7], len=3, cap=6
+	fmt.Println("a", a, len(a), cap(a))
+
+	newY:= changeSlice(a) 
+	fmt.Println("NewSlice: ", newSlice, len(newSlice),cap(newSlice)) // [1,2,3,4,10,6,7], len=7,cap=10
+	fmt.Println("NewY: ", newY, len(newY),cap(newY)) // [10,6,7,11], len=4,cap=6
+	fmt.Println("NewSlice: ", newSlice[0:8], len(newSlice),cap(newSlice)) // [1,2,3,4,10,6,7,11], len=7,cap=10
+
+
+	print(5,6,7,8,9)
+}
+
+func changeSlice(s []int) []int{
+ s[0]=10 // [10,6,7], len=3, cap=6
+ s = append(s, 11) // [10,6,7,11], len=4,cap=6
+ return s
+}
+
+// Variadic function
+func print(numbers ...int){
+ fmt.Println(numbers)
+ fmt.Println(len(numbers))
+	fmt.Println(cap(numbers))
 }
 
 /*
