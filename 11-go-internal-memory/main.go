6e732b9c9c58ddfb9289d24db0ee37dcc5dc76db
Sagor Majomder <sagormajomder@gmail.com>
2026-04-30 00:15:29 +0600

    Enhance comments in main.go to clarify memory allocation phases and function accessibility

--- 11-go-internal-memory/main.go
+++ 11-go-internal-memory/main.go
@@ -1,11 +1,14 @@
 package main
 import "fmt"
 
-const a=10
+const a=10 // store in code segment as it is read only value
 
 var p=100
 
 func call(){
+	// add() function also in code segment in the compile phase.
+	// call() function just reference it in its stack frame in the execution phase.
+	// As nested function, add() is bind with call(). So only call() function can access add() function. 
     add:=func (x int, y int){
         z:=x+y
         fmt.Println(z)
@@ -31,8 +34,10 @@ func init(){
 	2. execution phase
 
 	--> go run main.go => compile--> create a binary file named main--> then automatically execute the  binary file
+	--> go run main.go => compile => main => ./main
 
 	--> go build main.go => compile--> create a binary file named main --> then we can execute the binary file by running ./main
+	--> go build main.go => compile => main
 
 	--> Go is a compiled language, so it compiles the code before executing it.
 	-->In the compilation phase, all the constants and functions are allocated in the code segment(read-only). And all the global variables are allocated in the data segment.
@@ -43,7 +48,7 @@ func init(){
 /*
 -> program execution step by step
 	i) compilation phase
-		- syntax checking
+		- syntax checking (main function check, rules and regulation check etc)
 		- produce executable/binary file
 
 		
@@ -54,7 +59,7 @@ func init(){
 	data seg - check for global and static variables, if present allocate memory in data segment and initialize them
 
 		- check for init() function, if present execute it by allocating memory in stack segment called stack frame
-		***(Stack frame - memory allocated for a function call in stack segment)
+		***(Stack frame - memory allocated for a function execution in stack segment)
 		
 		- check for main() function, if present execute it by allocating memory in stack segment called stack frame
 		
@@ -64,3 +69,22 @@ func init(){
 	data lookup process -> local scope(stack segment) -> heap -> global scope(data segment) -> code segment
 
 	*/
+	
+//! Simulation 
+
+// ************* Compile Phase **************
+// **Code segment**
+/*
+		a=10
+		call = func() {....}
+		add = func () {....}
+		main = func () {....}
+		init = func () {....}
+*/
+
+// **Data Segment**
+/*
+	p=100
+*/
+
+	
