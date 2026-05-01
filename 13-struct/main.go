017d8b422c55f65eb2871aa3ff61e4f2a645ee86
Sagor Majomder <sagormajomder@gmail.com>
2026-05-01 11:19:47 +0600

    Enhance comments in main.go to clarify custom type usage and structure definitions

--- 13-struct/main.go
+++ 13-struct/main.go
@@ -2,20 +2,29 @@ package main
 
 import "fmt"
 
-// Custom Type
+// Custom Type (read only)
+// we can write first letter of custom types' name and property in both uppercase or lowercase.
+// It is just personal style
+// But use uppercase as most developer prefer that and it also make it able to export to other packages
 type User struct {
 	Name string // member variable or property
 	age  int
 }
 
+type employee struct{
+	name string
+	salary float64
+}
+
 func main() {
 
 	var user1 User
-	user1 = User{
+	user1 = User{ // Instance
 		Name: "Sagor",
 		age:  29,
 	}
 	fmt.Println(user1)
+
 
 	user2:= User{ // Instance
 		Name: "Mehedi",
@@ -24,4 +33,26 @@ func main() {
 	fmt.Println("Name: ", user2.Name)
 	fmt.Println("age: ", user2.age)
 
+	emp1:=employee{ // Instance
+		name: "Sagor",
+		salary: 50000,
+	}
+
+	fmt.Println(emp1)
+
 }
+
+//! Simulation 
+
+// ************* Compile Phase **************
+// **Code segment**
+/*
+		User = type User struct{...}
+		employee = type employee struct{...}
+		main = func(){...}
+*/
+
+// **Data Segment**
+/*
+	
+*/
